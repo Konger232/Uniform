81171905c1803e9c9689bb3c49554bb7d9b3c7d5
konger232 <yek089@g.harvard.edu>
2026-05-13 10:33:54 +0700

    404 redirect at the public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+/* Handle redirect to pulblic 404.html */
+const redirect = sessionStorage.getItem("redirect");
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
